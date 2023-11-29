ea6d166fb024eb1857b5fc80cbdaf74fb9707ef9
MostafaDidi <148250290+MostafaDidi@users.noreply.github.com>
2023-11-29 18:35:43 +0100

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,662 @@
+export const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/b73.jpg",
+    name: "three objects",
+   /* rating: {
+      stars: 4.5,
+      count: 87
+      
+    },*/
+    priceCents: 12000,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/b63.jpg",
+    name: "mid-day",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 11000,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/b92.jpg",
+    name: "peacefull stay",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 10000,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/products/b53.jpg",
+    name: "Reflecting the light",
+    rating: {
+      stars: 5,
+      count: 1
+    },
+    priceCents: 12000,
+    keywords: [
+      "toaster",
+      "kitchen",
+      "appliances"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/products/b4.jpg",
+    name: "Italien beauty",
+    rating: {
+      stars: 4,
+      count: 37
+    },
+    priceCents: 14000,
+    keywords: [
+      "plates",
+      "kitchen",
+      "dining"
+    ]
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "images/products/b5.jpg",
+    name: "Tranquille spot",
+    rating: {
+      stars: 4.5,
+      count: 175
+    },
+    priceCents: 10200,
+    keywords: [
+      "kitchen",
+      "cookware"
+    ]
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/products/b6.jpg",
+    name: "Plain to enjoy",
+    rating: {
+      stars: 4.5,
+      count: 317
+    },
+    priceCents: 13000,
+    keywords: [
+      "hoodies",
+      "sweaters",
+      "apparel"
+    ]
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: "images/products/b80.jpg",
+    name: "Moonlight",
+    rating: {
+      stars: 4.5,
+      count: 144
+    },
+    priceCents: 10000,
+    keywords: [
+      "bathroom",
+      "washroom",
+      "restroom",
+      "towels",
+      "bath towels"
+    ]
+  },
+  {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: "images/products/b50.jpg",
+    name: "les vacances en Grece",
+    rating: {
+      stars: 4.5,
+      count: 305
+    },
+    priceCents: 5000,
+    keywords: [
+      "bathroom",
+      "cleaning"
+    ]
+  },
+  {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: "images/products/b70.jpg",
+    name: "light in the sky",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 2000,
+    keywords: [
+      "shoes",
+      "running shoes",
+      "footwear"
+    ]
+  },
+  {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: "images/products/b10.jpg",
+    name: "Mistere",
+    rating: {
+      stars: 4.5,
+      count: 235
+    },
+    priceCents: 13000,
+    keywords: [
+      "robe",
+      "swimsuit",
+      "swimming",
+      "bathing",
+      "apparel"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  },
+  {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/products/b11.jpg",
+    name: "Round teapot ",
+    rating: {
+      stars: 4.5,
+      count: 30
+    },
+    priceCents: 2450,
+    keywords: [
+      "accessories",
+      "shades"
+    ]
+  },
+  {
+    id: "04701903-bc79-49c6-bc11-1af7e3651358",
+    image: "images/products/b12.jpg",
+    name: "Midday silence",
+    rating: {
+      stars: 4.5,
+      count: 562
+    },
+    priceCents: 15000,
+    keywords: [
+      "footwear",
+      "sandals",
+      "womens",
+      "beach",
+      "summer"
+    ]
+  },
+  {
+    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    image: "images/products/b13.jpg",
+    name: "Casablanca mosque",
+    rating: {
+      stars: 4.5,
+      count: 232
+    },
+    priceCents: 12000,
+    keywords: [
+      "bedroom",
+      "curtains",
+      "home"
+    ]
+  },
+  {
+    id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    image: "images/products/b66.jpg",
+    name: "Together",
+    rating: {
+      stars: 4,
+      count: 160
+    },
+    priceCents: 15000,
+    keywords: [
+      "shorts",
+      "apparel",
+      "mens"
+    ]
+  },
+  {
+    id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    image: "images/products/b15.jpg",
+    name: "Seascape",
+    rating: {
+      stars: 5,
+      count: 846
+    },
+    priceCents: 20000,
+    keywords: [
+      "water boiler",
+      "appliances",
+      "kitchen"
+    ]
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/b16.jpg",
+    name: "My beloved Imsouane",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 25000,
+    keywords: [
+      "kleenex",
+      "tissues",
+      "kitchen",
+      "tissues box",
+      "napkins"
+    ]
+  },
+  {
+    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    image: "images/products/b17.jpg",
+    name: "Patrimoine marocain",
+    rating: {
+      stars: 4,
+      count: 215
+    },
+    priceCents: 22000,
+    keywords: [
+      "hats",
+      "straw hats",
+      "summer",
+      "apparel"
+    ]
+  },
+  {
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: "images/products/b18.jpg",
+    name: "Hard labour",
+    rating: {
+      stars: 4.5,
+      count: 52
+    },
+    priceCents: 17990,
+    keywords: [
+      "jewelry",
+      "accessories",
+      "womens"
+    ]
+  },
+  {
+    id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    image: "images/products/b19.jpg",
+    name: "Asila",
+    rating: {
+      stars: 4.5,
+      count: 2465
+    },
+    priceCents: 13740,
+    keywords: [
+      "hooded",
+      "hoodies",
+      "sweaters",
+      "womens",
+      "apparel"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  },
+  {
+    id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+    image: "images/products/b30.jpg",
+    name: "Ready to make a decision",
+    rating: {
+      stars: 4.5,
+      count: 119
+    },
+    priceCents: 12500,
+    keywords: [
+      "bathmat",
+      "bathroom",
+      "home"
+    ]
+  },
+  {
+    id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+    image: "images/products/b88.jpg",
+    name: "Douar Atlas",
+    rating: {
+      stars: 4,
+      count: 326
+    },
+    priceCents: 2640,
+    keywords: [
+      "shoes",
+      "flats",
+      "womens",
+      "footwear"
+    ]
+  },
+  {
+    id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+    image: "images/products/b87.jpg",
+    name: "Double meaning",
+    rating: {
+      stars: 4.5,
+      count: 2556
+    },
+    priceCents: 15990,
+    keywords: [
+      "tshirts",
+      "shirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  },
+  {
+    id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    image: "images/products/b20.jpg",
+    name: "Funduk to rest in",
+    rating: {
+      stars: 4.5,
+      count: 2286
+    },
+    priceCents: 83000,
+    keywords: [
+      "garbage",
+      "bins",
+      "cans",
+      "kitchen"
+    ]
+  },
+  {
+    id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    image: "images/products/b21.jpg",
+    name: "Silver pot",
+    rating: {
+      stars: 4,
+      count: 456
+    },
+    priceCents: 23990,
+    keywords: [
+      "bedroom",
+      "bed sheets",
+      "sheets",
+      "covers",
+      "home"
+    ]
+  },
+  {
+    id: "d2785924-743d-49b3-8f03-ec258e640503",
+    image: "images/products/b22.jpg",
+    name: "Get started",
+    rating: {
+      stars: 5,
+      count: 83
+    },
+    priceCents: 12500,
+    keywords: [
+      "hats",
+      "winter hats",
+      "beanies",
+      "tuques",
+      "apparel",
+      "womens"
+    ]
+  },
+  {
+    id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
+    image: "images/products/b23.jpg",
+    name: "Taroudant",
+    rating: {
+      stars: 4.5,
+      count: 9017
+    },
+    priceCents: 22900,
+    keywords: [
+      "pants",
+      "apparel",
+      "mens"
+    ]
+  },
+  {
+    id: "1c079479-8586-494f-ab53-219325432536",
+    image: "images/products/b24.jpg",
+    name: "Eat, drink and enjoy",
+    rating: {
+      stars: 4,
+      count: 229
+    },
+    priceCents: 38900,
+    keywords: [
+      "shoes",
+      "running shoes",
+      "footwear",
+      "mens"
+    ]
+  },
+  {
+    id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    image: "images/products/b25.jpg",
+    name: "Contremplate before",
+    rating: {
+      stars: 3.5,
+      count: 42
+    },
+    priceCents: 16900,
+    keywords: [
+      "sunglasses",
+      "glasses",
+      "accessories",
+      "shades"
+    ]
+  },
+  {
+    id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    image: "images/products/b26.jpg",
+    name: "beauty in the fog",
+    rating: {
+      stars: 4.5,
+      count: 511
+    },
+    priceCents: 60000,
+    keywords: [
+      "cooking set",
+      "kitchen"
+    ]
+  },
+  {
+    id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    image: "images/products/b27.jpg",
+    name: "Imsouane harbour",
+    rating: {
+      stars: 4.5,
+      count: 130
+    },
+    priceCents: 16490,
+    keywords: [
+      "bathroom",
+      "washroom",
+      "mirrors",
+      "home"
+    ]
+  },
+  {
+    id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
+    image: "images/products/b28.jpg",
+    name: "tranquille water",
+    rating: {
+      stars: 4.5,
+      count: 248
+    },
+    priceCents: 24000,
+    keywords: [
+      "pants",
+      "sweatpants",
+      "jogging",
+      "apparel",
+      "womens"
+    ]
+  },
+  {
+    id: "d339adf3-e004-4c20-a120-40e8874c66cb",
+    image: "images/products/b29.jpg",
+    name: "Different destinies",
+    rating: {
+      stars: 4.5,
+      count: 117
+    },
+    priceCents: 24000,
+    keywords: [
+      "accessories",
+      "womens"
+    ]
+  },
+  {
+    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    image: "images/products/b86.jpg",
+    name: "Round flowers",
+    rating: {
+      stars: 4,
+      count: 126
+    },
+    priceCents: 28990,
+    keywords: [
+      "boxes",
+      "food containers",
+      "kitchen"
+    ]
+  },
+  {
+    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    image: "images/products/b31.jpg",
+    name: "Down to harbour",
+    rating: {
+      stars: 4.5,
+      count: 1211
+    },
+    priceCents: 22500,
+    keywords: [
+      "coffeemakers",
+      "kitchen",
+      "appliances"
+    ],
+  
+  },
+  {
+    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    image: "images/products/b32.jpg",
+    name: "Lion rock",
+    rating: {
+      stars: 4.5,
+      count: 363
+    },
+    priceCents: 30990,
+    keywords: [
+      "bedroom",
+      "home"
+    ]
+  },
+  {
+    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    image: "images/products/b3.jpg",
+    name: "Behind the dune",
+    rating: {
+      stars: 4.5,
+      count: 93
+    },
+    priceCents: 21100,
+    keywords: [
+      "bathroom",
+      "home",
+      "towels"
+    ]
+  },
+  {
+    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    image: "images/products/b34.jpg",
+    name: "Rabat capital",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 33900,
+    keywords: [
+      "shoes",
+      "running shoes",
+      "footwear",
+      "womens"
+    ]
+  },
+  {
+    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    image: "images/products/b35.jpg",
+    name: "Country after harvest",
+    rating: {
+      stars: 4,
+      count: 3
+    },
+    priceCents: 10747,
+    keywords: [
+      "food blenders",
+      "kitchen",
+      "appliances"
+    ]
+  },
+  {
+    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    image: "images/products/b36.jpg",
+    name: "Separating wall",
+    rating: {
+      stars: 5,
+      count: 679
+    },
+    priceCents: 39900,
+    keywords: [
+      "mixing bowls",
+      "baking",
+      "cookware",
+      "kitchen"
+    ]
+  },
+  {
+    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    image: "images/products/b37.jpg",
+    name: "Mediterranean quay",
+    rating: {
+      stars: 4.5,
+      count: 1045
+    },
+    priceCents: 57990,
+    keywords: [
+      "kitchen",
+      "kitchen towels",
+      "tissues"
+    ]
+  },
+  {
+    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
+    image: "images/products/b38.jpg",
+    name: "Japanees pot not for sale",
+    rating: {
+      stars: 4.5,
+      count: 3157
+    },
+    priceCents: 0,
+    keywords: [
+      "sweaters",
+      "hoodies",
+      "apparel",
+      "mens"
+    ]
+  }
+];
